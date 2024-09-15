68f15196950af98b1fd71ead8b31a3664dd2fee4
Rajat Singh <heylegend14@gmail.com>
2024-09-16 00:58:59 +0530

    Solved some week one easy and hard questions

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,9 +3,19 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
-
 function isAnagram(str1, str2) {
+  // If the lengths are not the same, they can't be anagrams
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+  // Convert both strings to lowercase to make the comparison case-insensitive
+  const sortedStr1 = str1.toLowerCase().split('').sort().join('');
+  const sortedStr2 = str2.toLowerCase().split('').sort().join('');
+
+  // If the sorted strings are equal, they are anagrams
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
+
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,16 +4,23 @@
   transactions is an array where each
   Transaction - an object like 
         {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
+  response = [];
+
+  for (let transaction of transactions) {
+    const category = transaction.category;
+    const price = transaction.price;
+
+  }
   return [];
 }
 
